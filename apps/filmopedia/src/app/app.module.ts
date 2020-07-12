0ffc8bda06c914e3d7b99c262de94aef4d3ca695
David RJ <david.rud135@gmail.com>
2020-07-12 15:54:10 +0300

    Added Nebular UI kit to the project

--- apps/filmopedia/src/app/app.module.ts
+++ apps/filmopedia/src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http';
+import { NbThemeModule } from '@nebular/theme';
 
 import { AppComponent } from './app.component';
 
-import { PrimeNGModule } from './modules/primeng.module';
 import { AppRoutingModule } from './app-routing.module';
+import { NebularUIModule } from './modules/nebular-ui.module';
 
 @NgModule({
   declarations: [AppComponent],
@@ -15,7 +16,8 @@ import { AppRoutingModule } from './app-routing.module';
     BrowserAnimationsModule,
     HttpClientModule,
     AppRoutingModule,
-    PrimeNGModule,
+    NbThemeModule.forRoot({ name: 'cosmic' }),
+    NebularUIModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
