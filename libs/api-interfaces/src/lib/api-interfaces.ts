0c6c00fd6ed59b5177085663fa6dfc4cd4dce36e
David RJ <david.rud135@gmail.com>
2020-07-14 01:03:35 +0300

    Created interface for Movie object

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -1,3 +1,18 @@
 export interface Message {
   message: string;
 }
+
+/**
+ * @property {string} name - name of the movie.
+ * @property {string[]} genres - list of movie's genres.
+ * @property {string} year - year when the movie came out.
+ * @property {string[]} countries - list of countries which has developed the movie.
+ * @property {string} posterURL - URL of the movie poster image.
+ */
+export interface Movie {
+  name: string;
+  genres: string[];
+  year: string;
+  countries: string[];
+  posterURL: string;
+}
