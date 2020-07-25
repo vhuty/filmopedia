bf288ca56bff60eaf6723c179308fb1a196821cd
David RJ <david.rud135@gmail.com>
2020-07-26 00:57:51 +0300

    Displaying real TMDB popular movies on the home page

--- apps/filmopedia/src/app/core/movies.service.ts
+++ apps/filmopedia/src/app/core/movies.service.ts
@@ -1,24 +1,26 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { BehaviorSubject, Observable } from 'rxjs';
-import { pluck } from 'rxjs/operators';
+import { pluck, switchMap } from 'rxjs/operators';
 
-import { Genre } from '@filmopedia/api-interfaces';
+import { Genre, MoviesResponse } from '@filmopedia/api-interfaces';
 import { environment } from './../../environments/environment';
 
 @Injectable({ providedIn: 'root' })
 export class MoviesService {
   private readonly baseURL = 'https://api.themoviedb.org/3';
-  private favoriteMovies$ = new BehaviorSubject<string[]>([]);
+  private favoriteMovies$ = new BehaviorSubject<number[]>([]);
+  private currMoviesPage$ = new BehaviorSubject<number>(1);
 
   constructor(private http: HttpClient) {
     const favMoviesStr = localStorage.getItem('favoriteMovies');
     if (favMoviesStr) {
       this.favoriteMovies$.next(JSON.parse(favMoviesStr));
     }
-    this.favoriteMovies$.subscribe((moviesIds: string[]) => {
+    this.favoriteMovies$.subscribe((moviesIds: number[]) => {
       localStorage.setItem('favoriteMovies', JSON.stringify(moviesIds));
     });
+    this.currMoviesPage$.subscribe();
   }
 
   public getMoviesGenres(): Observable<Genre[]> {
@@ -27,19 +29,33 @@ export class MoviesService {
       .pipe(pluck('genres'));
   }
 
-  public getFavoriteMovies(): Observable<string[]> {
+  public getMovies(): Observable<MoviesResponse> {
+    return this.currMoviesPage$.pipe(
+      switchMap((page: number) =>
+        this.http.get<MoviesResponse>(
+          `${this.baseURL}/movie/popular?api_key=${environment.apiKey}&page=${page}`
+        )
+      )
+    );
+  }
+
+  public getFavoriteMovies(): Observable<number[]> {
     return this.favoriteMovies$.asObservable();
   }
 
-  public addMovieToFavorites(movieId: string): void {
+  public addMovieToFavorites(movieId: number): void {
     const favMoviesIds = this.favoriteMovies$.getValue();
     favMoviesIds.push(movieId);
     this.favoriteMovies$.next(favMoviesIds);
   }
 
-  public removeMovieFromFavorites(movieId: string): void {
+  public removeMovieFromFavorites(movieId: number): void {
     const favMoviesIds = this.favoriteMovies$.getValue();
     favMoviesIds.splice(favMoviesIds.indexOf(movieId), 1);
     this.favoriteMovies$.next(favMoviesIds);
+  }
+
+  public changeMoviesPage(page: number): void {
+    this.currMoviesPage$.next(page);
   }
 }
