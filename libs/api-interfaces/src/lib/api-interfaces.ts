9f9f071b9e653ab36e2a0a744278c4694d65e996
David RJ <david.rud135@gmail.com>
2020-08-04 12:44:16 +0300

    Created 'Movie Details' and 'Response Error' interfaces

--- libs/api-interfaces/src/lib/api-interfaces.ts
+++ libs/api-interfaces/src/lib/api-interfaces.ts
@@ -5,6 +5,63 @@ export interface MoviesResponse {
   results: Movie[];
 }
 
+export interface MovieDetails {
+  id: number;
+  imdb_id: string | null;
+  homepage: string | null;
+  adult: boolean;
+  video: boolean;
+  title: string;
+  budget: number;
+  revenue: number;
+  popularity: number;
+  vote_average: number;
+  release_date: string;
+  vote_count: number;
+  runtime: number | null;
+  tagline: string | null;
+  poster_path: string | null;
+  backdrop_path: string | null;
+  original_title: string;
+  original_language: string;
+  overview: string | null;
+  status:
+    | 'Rumored'
+    | 'Planned'
+    | 'In Production'
+    | 'Post Production'
+    | 'Released'
+    | 'Canceled';
+  genres: Genre[];
+  production_companies: {
+    id: number;
+    name: string;
+    logo_path: string | null;
+    origin_country: string;
+  }[];
+  production_countries: {
+    iso_3166_1: string;
+    name: string;
+  }[];
+  spoken_languages: {
+    iso_639_1: string;
+    name: string;
+  }[];
+  belongs_to_collection: object | null;
+  videos?: {
+    results: {
+      id: string;
+      key: string;
+      name: string;
+      site: string;
+      size: number;
+      type: string;
+      iso_639_1: string;
+      iso_3166_1: string;
+    }[];
+  };
+}
+
 export interface Movie {
   id: number;
   title: string;
@@ -28,4 +85,9 @@ export interface Movie {
 export interface Genre {
   id: number;
   name: string;
+}
+
+export interface ResponseError {
+  status_code: number;
+  status_message: string;
 }
