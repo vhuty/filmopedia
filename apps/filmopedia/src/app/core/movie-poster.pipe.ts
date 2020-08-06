df3037361cb79664df1b51a0643c5eb4c951741f
David RJ <david.rud135@gmail.com>
2020-08-06 14:03:22 +0300

    Created movie poster pipe to handle movies posters

---
+++ apps/filmopedia/src/app/core/movie-poster.pipe.ts
@@ -0,0 +1,20 @@
+import { Pipe, PipeTransform } from '@angular/core';
+import { Movie, MovieDetails } from '@filmopedia/api-interfaces';
+
+@Pipe({
+  name: 'moviePoster',
+})
+export class MoviePosterPipe implements PipeTransform {
+  transform(
+    movie: Movie | MovieDetails,
+    posterSize: 'regular' | 'small' = 'regular'
+  ): string {
+    const primaryPosterPath = movie.poster_path || movie.backdrop_path;
+    if (!primaryPosterPath) {
+      return '../../assets/images/poster-not-available.jpg';
+    }
+    return posterSize === 'regular'
+      ? `https://image.tmdb.org/t/p/w440_and_h660_face${primaryPosterPath}`
+      : `https://image.tmdb.org/t/p/w220_and_h330_face${primaryPosterPath}`;
+  }
+}
